62047ac87a58ada5011d721421e020cff1b561fa
BaeKyeongMan <b991212@naver.com>
2026-02-23 21:43:17 +0900

    (2026-02-23)

---
+++ Program/Program/Intelligence.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "Intelligence.h"
+
+void Intelligence::Enhance()
+{
+	cout << "your Intelligence has increased :  " << endl;
+}
---
+++ Program/Program/Stat.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "Stat.h"
+
+
+void Stat::Enhance()
+{
+	cout << "your Stat has increased :  " << endl;
+}
